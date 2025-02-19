681bb00b413ff87ae1082c63c97804fbaa2b7840
404wizard <the404wizard@protonmail.com>
2025-02-19 20:51:46 +0300

    Add notations.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import { EvmChain } from "@moralisweb3/common-evm-utils";
 const app = express();
 const port = 3000;
 const API_KEY = process.env.API_KEY;
+
+// API URL to get Wallet information
 const walletBalanceURL = "https://deep-index.moralis.io/api/v2.2/wallets/:address/tokens"
 
 app.use(express.static("public"));
@@ -25,23 +27,20 @@ app.post("/search", async (req, res) => {
                     "x-api-key" : API_KEY,
                 },
                 params : {
-                    "chain" : "0x89",
-                    "exclude_spam": true,
-                    "exclude_unverified_contracts": true,
+                    "chain" : "0x89", // Polygon blockchain
+                    "exclude_spam": true, // Auto filter out spam coins
+                    "exclude_unverified_contracts": true, // Auto filter out potential spam coins
                     "address" : req.body.walletadd,
                 }
             });
-            
             res.render("index.ejs", { content: response.data });
         } catch (e) {
             console.error(e);
             res.render("index.ejs", { error: e.message })
         };        
-    } else {
+    } else { // If the wallet address field is empty return message.
         res.render("index.ejs", { error: "You forgot to enter your wallet address. Try again!" });
     }
-    
-
 });
 
 app.listen(port, () => {
